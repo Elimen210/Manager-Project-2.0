bc5f5eb4a9e0507f59d3d3523dc2759c242696a3
Elie MEN <133395047+Elimen210@users.noreply.github.com>
2025-09-24 19:35:14 +0100

    Add initial answer flow
    Introduces a new Response model and ResponseForm for handling user answers. Updates index.html to use a form for answer submission and adds a new view and template (adding-page.html) for the first answer flow. Updates URLs and styles, and improves menu layout and input handling.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
+from tkinter import FIRST
 from django.urls import path
 from . import views
-from .views import Index
+from .views import Index, FirstAnswer
 
 urlpatterns = [
     path('', Index.as_view(), name='landing'),
+    path('1/', FirstAnswer.as_view(), name='FAR')
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from .models import Response
+from .forms import ResponseForm
 
 class Index(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'core/index.html')
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            response = ResponseForm(request.POST)
+            if response.is_valid():
+                answer = response.cleaned_data['answer']
+                if answer == 1:
+                    response.save()
+                    return redirect('FAR') # FAR == First Answer Render
+        else:
+            response = ResponseForm()
+        return render(request, 'core/index.html')
+
+class FirstAnswer(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'core/adding-page.html')
+
+    def post(self, request, *args, **kwargs):
+        return render(request, 'core/adding-page.html')
